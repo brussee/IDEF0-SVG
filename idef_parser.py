3283815759f3bea91b98d2191d04421619bcb712
Paul Brussee <P.W.G.Brussee@tudelft.nl>
2023-03-18 02:41:19 +0000

    refactor: code formatting

--- idef_parser.py
+++ idef_parser.py
@@ -1,7 +1,9 @@
 from parsimonious.grammar import Grammar
 from parsimonious.nodes import NodeVisitor
 
-grammar = Grammar(r"""
+
+grammar = Grammar(
+    r"""
     expr        = (function / emptyline)*
     function    = block port+
 
@@ -13,60 +15,60 @@ grammar = Grammar(r"""
     quoted      = ~'\[[^\[]+\]'
     ws          = ~"\s*"
     emptyline   = ws+
-""")
+"""
+)
 
 
 def typeToAction(type):
-    if type == 'in':
-        return 'receives'
-    elif type == 'res':
-        return 'respects'
-    elif type == 'out':
-        return 'produces'
-    elif type == 'ctrl':
-        return 'requires'
-    elif type == 'comp':
-        return 'is compsed of'
+    if type == "in":
+        return "receives"
+    elif type == "res":
+        return "respects"
+    elif type == "out":
+        return "produces"
+    elif type == "ctrl":
+        return "requires"
+    elif type == "comp":
+        return "is compsed of"
     else:
-        raise TypeError('Not part of IDEF0 keyword')
+        raise TypeError("Not part of IDEF0 keyword")
 
 
 class IdefVisitor(NodeVisitor):
-
     def visit_expr(self, node, visited_children):
-        """ Returns the overall output. """
-        output = ''
+        """Returns the overall output."""
+        output = ""
         for child in visited_children:
             output += child[0]
         return output
 
     def visit_function(self, node, visited_children):
-        """ Makes a dict of the section (as key) and the key/value pairs. """
+        """Makes a dict of the section (as key) and the key/value pairs."""
         function_name, ports = visited_children
 
-        output = ''
+        output = ""
         for port in ports:
             action = typeToAction(port[0])
-            output += f'{function_name} {action} {port[1]}\n'
+            output += f"{function_name} {action} {port[1]}\n"
 
         return output
 
     def visit_block(self, node, visited_children):
-        """ Gets the section name. """
+        """Gets the section name."""
         quoted, *_ = visited_children
         return quoted.text[1:-1].title()
 
     def visit_port(self, node, visited_children):
-        """ Gets each key/value pair, returns a tuple. """
+        """Gets each key/value pair, returns a tuple."""
         type, _, value, *_ = node.children
         return type.text, value.text
 
     def generic_visit(self, node, visited_children):
-        """ The generic visit method. """
+        """The generic visit method."""
         return visited_children or node
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     data = """[CookPizza]
     in Ingredients
     res CustomerOrder
--- webapp.py
+++ webapp.py
@@ -1,24 +1,26 @@
 import subprocess as p
 
-from flask import Flask, Response, abort
+from flask import Flask, Response
 
+from idef_parser import IdefVisitor, grammar
 from plantuml_decoder import plantuml_decode
-from parser import grammar, IdefVisitor
+
 
 app = Flask(__name__)
 
 
-@app.route('/svg/<base64_data>')
+@app.route("/svg/<base64_data>")
 def render(base64_data):
     decoded = plantuml_decode(base64_data)
     tree = grammar.parse(decoded.lstrip())
     iv = IdefVisitor()
     parsed = iv.visit(tree)
 
-    process = p.Popen('./bin/schematic', stdin=p.PIPE, stdout=p.PIPE)
-    svg_data = process.communicate(parsed.encode('utf-8'))[0]
+    process = p.Popen("./bin/schematic", stdin=p.PIPE, stdout=p.PIPE)
+    svg_data = process.communicate(parsed.encode("utf-8"))[0]
 
-    return Response(svg_data, mimetype='image/svg+xml')
+    return Response(svg_data, mimetype="image/svg+xml")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  # , debug=True)
